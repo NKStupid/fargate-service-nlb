8f306134a4f50621464a22699d76ecad75e5d89d
NKStupid <64253633+NKStupid@users.noreply.github.com>
2021-01-03 22:33:07 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -32,77 +32,77 @@ class FargateServiceNLB extends cdk.Stack {
     //4. Create the ECS fargate cluster
     const cluster = new ecs.Cluster(this, 'wise-demo-cluster', { vpc, clusterName: "wise-demo-cluster" });
 
-//     //5. Create a task definition for our cluster to invoke a task
-//     const taskDef = new ecs.FargateTaskDefinition(this, "search-api-task", {
-//       family: 'search-api-task',
-//       memoryLimitMiB: 512,
-//       cpu: 256,
-//       executionRole: execRole,
-//       taskRole: execRole
-//     });
+    //5. Create a task definition for our cluster to invoke a task
+    const taskDef = new ecs.FargateTaskDefinition(this, "wise-demo-task", {
+      family: 'wise-demo-task',
+      memoryLimitMiB: 512,
+      cpu: 256,
+      executionRole: execRole,
+      taskRole: execRole
+    });
 
-//     //6. Create log group for our task to put logs
-//     const lg = LogGroup.fromLogGroupName(this, 'search-api-log-group',  '/ecs/search-api-task');
-//     const log = new ecs.AwsLogDriver({
-//       logGroup : lg? lg : new LogGroup(this, 'search-api-log-group',{logGroupName:'/ecs/search-api-task'
-//       }),
-//       streamPrefix : 'ecs'
-//     })
+    //6. Create log group for our task to put logs
+    const lg = LogGroup.fromLogGroupName(this, 'wise-demo-log-group',  '/ecs/wise-demo-task');
+    const log = new ecs.AwsLogDriver({
+      logGroup : lg? lg : new LogGroup(this, 'wise-demo-log-group',{logGroupName:'/ecs/wise-demo-task'
+      }),
+      streamPrefix : 'ecs'
+    })
 
-//     //7. Create container for the task definition from ECR image
-//     var container = taskDef.addContainer("search-api-container", {
-//       image: ecs.ContainerImage.fromRegistry("487213271675.dkr.ecr.us-west-2.amazonaws.com/search-api:latest"),
-//       logging:log
-//     })
+    //7. Create container for the task definition from ECR image
+    var container = taskDef.addContainer("wise-demo-container", {
+      image: ecs.ContainerImage.fromRegistry("526913279474.dkr.ecr.us-east-1.amazonaws.com/ecr-simplehttp:latest"),
+      logging:log
+    })
 
-//     //8. Add port mappings to your container...Make sure you use TCP protocol for Network Load Balancer (NLB)
-//     container.addPortMappings({
-//       containerPort: 7070,
-//       hostPort: 7070,
-//       protocol: ecs.Protocol.TCP
-//     });
+    //8. Add port mappings to your container...Make sure you use TCP protocol for Network Load Balancer (NLB)
+    container.addPortMappings({
+      containerPort: 8000,
+      hostPort: 8000,
+      protocol: ecs.Protocol.TCP
+    });
 
-//     //9. Create the NLB using the above VPC.
-//     const lb = new NetworkLoadBalancer(this, 'search-api-nlb', {
-//       loadBalancerName: 'search-api-nlb',
-//       vpc,
-//       internetFacing: false
-//     });
+    //9. Create the NLB using the above VPC.
+    const lb = new NetworkLoadBalancer(this, 'wise-demo-nlb', {
+      loadBalancerName: 'wise-demo-nlb',
+      vpc,
+      internetFacing: false
+    });
 
-//     //10. Add a listener on a particular port for the NLB
-//     const listener = lb.addListener('search-api-listener', {
-//       port: 7070,
-//     });
+    //10. Add a listener on a particular port for the NLB
+    const listener = lb.addListener('wise-demo-listener', {
+      port: 8000,
+    });
 
-//     //11. Create your own security Group using VPC
-//     const secGroup = new SecurityGroup(this, 'search-api-sg', {
-//       securityGroupName: "search-sg",
-//       vpc:vpc,
-//       allowAllOutbound:true
-//     });
+    //11. Create your own security Group using VPC
+    const secGroup = new SecurityGroup(this, 'wise-demo-sg', {
+      securityGroupName: "wise-demo-sg",
+      vpc:vpc,
+      allowAllOutbound:true
+    });
 
-//     //12. Add IngressRule to access the docker image on 80 and 7070 ports 
-//     secGroup.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(80), 'SSH frm anywhere');
-//     secGroup.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(7070), '');
+    //12. Add IngressRule to access the docker image on 80 and 8000 ports 
+    secGroup.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(80), 'SSH frm anywhere');
+    secGroup.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(8000), '');
 
-//     //13. Create Fargate Service from cluster, task definition and the security group
-//     const fargateService = new ecs.FargateService(this, 'search-api-fg-service', {
-//       cluster,
-//       taskDefinition: taskDef, 
-//       assignPublicIp: true, 
-//       serviceName: "search-api-svc",
-//       securityGroup:secGroup
-//     });
+    //13. Create Fargate Service from cluster, task definition and the security group
+    const fargateService = new ecs.FargateService(this, 'wise-demo-fg-service', {
+      cluster,
+      taskDefinition: taskDef, 
+      assignPublicIp: true, 
+      serviceName: "wise-demo-svc",
+      securityGroup:secGroup
+    });
 
-//     //14. Add fargate service to the listener 
-//     listener.addTargets('search-api-tg', {
-//       targetGroupName: 'search-api-tg',
-//       port: 7070,
-//       targets: [fargateService],
-//       deregistrationDelay: cdk.Duration.seconds(300)
-//     });
+    //14. Add fargate service to the listener 
+    listener.addTargets('wise-demo-tg', {
+      targetGroupName: 'wise-demo-tg',
+      port: 8000,
+      targets: [fargateService],
+      deregistrationDelay: cdk.Duration.seconds(300)
+    });
 
-//     new cdk.CfnOutput(this, 'ClusterARN: ', { value: cluster.clusterArn });
+    new cdk.CfnOutput(this, 'ClusterARN: ', { value: cluster.clusterArn });
   }
 }
 
